e12d0d722ceb2a13c89fcd5b24b14039bcd2d88e
Denis13s <141165300+Denis13s@users.noreply.github.com>
2023-12-20 10:37:30 +0100

    Initial Commit

---
+++ HW-4.3-DY/HW_4_3_DYApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_4_3_DYApp.swift
+//  HW-4.3-DY
+//
+//  Created by Denis Yarets on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct HW_4_3_DYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
